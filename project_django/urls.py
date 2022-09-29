2b43775eb6b7a0d70a3c3a61ea2e5b56885e5512
jihanifah <jihanhanifahyasmin12@gmail.com>
2022-09-29 16:34:36 +0700

    finished lab 4

--- project_django/urls.py
+++ project_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wishlist/', include('wishlist.urls')),
+    path('example_app/', include('example_app.urls') )
 ]
